17b0f6d850839cc2fef35050636031783df25d32
Taylor Masiello <taylor.masiello@outlook.com>
2022-12-21 21:31:26 -0800

    builds, runs, note A plays

--- SoundSynth/main.cpp
+++ SoundSynth/main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 
 double MakeNoise(double dTime) // dTime = time passed since start of program
 {
-
+	// amplitude (0.5) * sin(frequency in hrtz * dTime)
+	// 440 hrtz should be the note A
+	// sin takes angular velocity, not hrtz, so conversion (value * 2 * pi)
+	// "at given point in time (dTime), what should our sin wave look like"
+	return 0.5 * sin(440.0 * (2 * 3.14159) * dTime);
 }
 
 
@@ -22,6 +26,19 @@ int main()
 
 	// Create sound machine
 	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+
+	// Link noise function with sound machine
+	// using a function pointer, as noise maker class constantly running in bckgrnd, thus dTime continuously increasing
+	// will return a value between 0 and 1 to represent where the speaker cone should be (relative to time)
+	sound.SetUserFunction(MakeNoise);
+
+
+	while (1)
+	{
+
+	}
+
+
 
 	return 0;
 }
--- SoundSynth/olcNoiseMaker.h
+++ SoundSynth/olcNoiseMaker.h
@@ -1,3 +1,18 @@
+/*
+	OneLoneCoder.com - Simple Audio Noisy Thing
+	"Allows you to simply listen to that waveform!" - @Javidx9
+	License
+	~~~~~~~
+	Copyright (C) 2018  Javidx9
+	This program comes with ABSOLUTELY NO WARRANTY.
+	This is free software, and you are welcome to redistribute it
+	under certain conditions; See license for details.
+	Original works located at:
+	https://www.github.com/onelonecoder
+	https://www.onelonecoder.com
+	https://www.youtube.com/javidx9
+*/
+
 #pragma once
 
 #pragma comment(lib, "winmm.lib")
