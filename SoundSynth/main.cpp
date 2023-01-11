2b749674bd202b1a9e5ae02a572efa29a82280f8
Taylor Masiello <taylor.masiello@outlook.com>
2023-01-10 17:31:37 -0800

    Sound Synthesizer V2.0 updates; currently single sine wave oscillator; working next to make multiple oscillators with single amplitude envelope

--- SoundSynth/main.cpp
+++ SoundSynth/main.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
-
-atomic<double> dFrequencyOutput = 0.0;
-
-
-double MakeNoise(double dTime) // dTime = time passed since start of program
+// Converts frequency (Hz) to angular velocity
+double w(double dHertz)
 {
-	// amplitude (0.5) * sin(frequency in hrtz * dTime)
-	// sin takes angular velocity, not hrtz, so conversion (value * 2 * pi)
-	// dOutput captures sin wave shape at given point in time (dTime)
-	double dOutput = 1.0 * sin(dFrequencyOutput * (2 * 3.14159) * dTime);
+	return dHertz * 2.0 *PI;
+}
 
-	// takes dOutput sin wave and makes it into square wave
-	// we set amplitude implicitly as can only have a + or - in a square wave
-	if (dOutput > 0.0)
-		return 0.2;
-	else
-		return -0.2;
+// Global synthesizer variables
+atomic<double> dFrequencyOutput = 0.0;			// dominant output frequency of instrument, i.e. the note
+double dOctaveBaseFrequency = 110.0; // A2		// frequency of octave represented by keyboard
+double d12thRootOf2 = pow(2.0, 1.0 / 12.0);		// assuming western 12 notes per ocatve
+
+// Function used by olcNoiseMaker to generate sound waves
+// Returns amplitude (-1.0 to +1.0) as a function of time
+double MakeNoise(double dTime)
+{
+	double dOutput = sin(w(dFrequencyOutput) * dTime);
+
+	return dOutput * 0.4; // Master Volume
 
 }
 
@@ -33,43 +34,55 @@ int main()
 
 	// Display findings
 	for (auto d : devices) wcout << "Found Output Device: " << d << endl;
+	wcout << "Using Device: " << devices[0] << endl;
+
+	// Display a keyboard
+	wcout << endl <<
+		"|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |" << endl <<
+		"|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |" << endl <<
+		"|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__" << endl <<
+		"|     |     |     |     |     |     |     |     |     |     |" << endl <<
+		"|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
+		"|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
 
 	// Create sound machine
 	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
 
 	// Link noise function with sound machine
-	// using a function pointer, as noise maker class constantly running in bckgrnd, thus dTime continuously increasing
-	// will return a value between 0 and 1 to represent where the speaker cone should be (relative to time)
 	sound.SetUserFunction(MakeNoise);
 
-	// every doubling frequency moves up 1 octave; 12 notes in an octave
-	// can't simply divide frequency by 12 as it doubles each time
-	// instead using power of 2 to the 12th root
-	double dOctaveBaseFrequency = 110.0; // A2
-	double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
-
+	// Sit in loop, capturing keyboard state changes and modify synthesizer output accordingly
+	int nCurrentKey = -1;
+	bool bKeyPressed = false;
 
 	while (1)
 	{
-		// Add a keyboard like a piano
-
-		bool bKeyPressed = false;
-		for (int k = 0; k < 15; k++)
+		bKeyPressed = false;
+		for (int k = 0; k < 16; k++)
 		{
-			// testing if highest bit of a list key is present; if yes, it's pressed
-			// note mapping: Z is a; S is aSharp; X is b; C is c; F is cSharp
-			// V is d; G is dSharp; B is e; N is f; J is fSharp; M is g; K is gSharp
-			// unsure what notes , (xbc) or L or . (xbe) are
 			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe"[k])) & 0x8000)
 			{
-				// 440 hrtz should be the note A
-				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+				if (nCurrentKey != k)
+				{
+					dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+					wcout << "\nNote On: " << sound.GetTime() << "s" << dFrequencyOutput << "Hz";
+					nCurrentKey = k;
+				}
+
 				bKeyPressed = true;
 			}
 		}
 
 		if (!bKeyPressed)
 		{
+			{
+				if (nCurrentKey != -1)
+				{
+					wcout << "\rNote Off: " << sound.GetTime() << "s                        ";
+					nCurrentKey = -1;
+				}
+			}
+			
 			dFrequencyOutput = 0.0;
 		}
 
